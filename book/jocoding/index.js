323782bfb58702a0bc0f6b6d8ac06583b76f285e
BY-ee <gjwjqgh3@naver.com>
2024-08-01 00:28:07 +0900

    익스프레스를 활용한 백엔드 서버 구현

--- book/jocoding/index.js
+++ book/jocoding/index.js
@@ -1,6 +1,6 @@
 const figlet = require('figlet');
 
-figlet("figlet", function (err, data) {
+figlet("figletS", function (err, data) {
     if (err) {
         console.log('Something went wrong...');
         console.error(err);
